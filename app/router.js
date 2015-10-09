a00255337ca6b7225f10a6fd3b09ba7f812f2483
Michael Josephson <mikej@users.noreply.github.com>
2015-10-09 15:20:20 +0100

    add note_id to note route

--- app/router.js
+++ app/router.js
@@ -8,9 +8,9 @@ var Router = Ember.Router.extend({
 Router.map(function() {
   this.route('register');
   this.route('login');
-  this.route('notebooks', { path: 'notebooks/:user_id'}, function() {
-    this.route("notes", { path:"notes/:notebook_id"}, function() {
-      this.route('note');
+  this.route('notebooks', { path:'notebooks/:user_id'}, function() {
+    this.route("notes", { path:'notes/:notebook_id'}, function() {
+      this.route('note', { path:'note/:note_id'}, function(){});
     });
   });
 });
