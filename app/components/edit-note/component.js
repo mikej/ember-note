1d08203e60ca20593737604955c7b53e233c20dd
Michael Josephson <mikej@users.noreply.github.com>
2015-10-23 15:41:11 +0100

    add actions to edit-note component

--- app/components/edit-note/component.js
+++ app/components/edit-note/component.js
@@ -1,5 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  tagName: "span"
+  tagName: "span",
+  actions: {
+    saveNote: function() {
+      this.get('note').save();
+    },
+    closeNote: function() {
+      this.sendAction('close');
+    }
+  }
 });
